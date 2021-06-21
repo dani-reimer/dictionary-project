2397f47edf2dc4ed348bea1403d83744a33afee8
dani-reimer <78756494+dani-reimer@users.noreply.github.com>
2021-06-21 10:52:11 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="#" className="btn btn-primary shadow"> Click Here </a>
       </header>
     </div>
   );
